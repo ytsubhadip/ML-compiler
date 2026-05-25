5c1e60461f1dfdf9a7b16e6c6a365b40c6ebf404
subhadip <ytsubhadip0099@gmail.com>
2026-05-25 22:07:26 +0530

    route update

--- frontend/server.js
+++ frontend/server.js
@@ -1,19 +1,18 @@
 const express = require("express")
 const path = require('path')
 const bodyP = require('body-parser')
-const compiler = require('compilex')
-
-var option = { stats: true }
-compiler.init(option)
-compiler.init({ stats: true });
-
 const app = express()
 
 app.use(bodyP.json())
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "public")));
 
+// Import Routes
+const compileRoutes = require("./routes/compile");
 
+// Use Routes
+// mount compile router at root so its internal '/compiler' path becomes '/compiler'
+app.use("/", compileRoutes);
 
 app.get("/", (req, res) => {
 
@@ -26,91 +25,10 @@ app.get("/ide", function (req, res) {
 })
 
 
-app.post("/compiler", function (req, res) {
-    const code = req.body.code
-    const input = req.body.input
-    const lang = req.body.lang
-    try {
-        if (lang == "CPP") {
-            if (!input) {
-                var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } };
-                compiler.compileCPP(envData, code, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        res.send({ output: "error" })
-                    }
 
-                });
-            }
-            else {
-                var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } };
-                compiler.compileCPPWithInput(envData, code, input, function (data) {
-                    if (data.output) {
-                        res.send(data)
-                    }
-                    else {
-                        res.send({ output: "error" })
-                    }
-                });
-            }
-        }
-        else if (lang == "java") {
-            if (!input) {
-                var envData = { OS: "windows", options: { timeout: 10000 } };
-                compiler.compileJava(envData, code, function (data) {
-                    if (data.output) {
-                        res.send(data)
-                    }
-                    else {
-                        res.send({ output: "error" })
-                    }
-                });
-            }
-            else {
-                var envData = { OS: "windows", options: { timeout: 10000 } };
-                compiler.compileJavaWithInput(envData, code, input, function (data) {
-                    if (data.output) {
-                        res.send(data)
-                    }
-                    else {
-                        res.send({ output: "error" })
-                    }
-                });
-            }
-        }
-        else if (lang == 'python') {
-            if (!input) {
-                var envData = { OS: "windows" };
-                compiler.compilePython(envData, code, function (data) {
-                    if (data.output) {
-                        res.send(data)
-                    }
-                    else {
-                        res.send({ output: "error" })
-                    }
+app.get("/status", (req, res) => {
+    res.json({ status: "ok", uptime: process.uptime() });
+});
 
-                });
-            }
-            else {
-                var envData = { OS: "windows" };
-                compiler.compilePythonWithInput(envData, code, input, function (data) {
-                    if (data.output) {
-                        res.send(data)
-                    }
-                    else {
-                        res.send({ output: "error" })
-                    }
-                });
-            }
-
-        }
-    }
-    catch (e) {
-        console.log("error")
-    }
-
-})
 
 app.listen(8000)
